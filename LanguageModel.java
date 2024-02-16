96bf1625c1d47f09b64eba188ae2e0524e39112a
nschumac <schumachernikolai@gmail.com>
2024-02-16 13:16:46 +0200

    initial push

--- LanguageModel.java
+++ LanguageModel.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Random;
+import java.util.random.RandomGenerator;
 
 public class LanguageModel {
 
@@ -33,18 +34,42 @@ public class LanguageModel {
 
     /** Builds a language model from the text in the given file (the corpus). */
 	public void train(String fileName) {
-		// Your code goes here
+
+        for (int i = 0; i < fileName.length() - windowLength ; ++i) {
+            String key = fileName.substring(i, windowLength);
+            if (CharDataMap.get(key) == null) {
+                CharDataMap.put(key, new List());
+            }
+            CharDataMap.get(key).update(fileName.charAt(i + windowLength));
+        }
 	}
 
     // Computes and sets the probabilities (p and cp fields) of all the
 	// characters in the given list. */
 	public void calculateProbabilities(List probs) {				
 		// Your code goes here
+        int totalLength = probs.getSize();
+        double cp = 0.0;
+        for (int i = 0; i < totalLength; ++i) {
+            CharData currentCharData = probs.get(i);
+            currentCharData.p = (double)currentCharData.count / totalLength;
+            currentCharData.cp = (double)cp + currentCharData.p;
+            cp += currentCharData.p;
+        }
 	}
 
     // Returns a random character from the given probabilities list.
 	public char getRandomChar(List probs) {
-		// Your code goes here
+        double cpValue = randomGenerator.nextDouble();
+        CharData[] charDataArray = probs.toArray();
+
+        for (CharData cd : charDataArray) {
+            if (cd.cp > cpValue) {
+                return cd.chr;
+            }
+        }
+        // will never get here
+        return ' '; 
 	}
 
     /**
@@ -56,7 +81,15 @@ public class LanguageModel {
 	 */
 	public String generate(String initialText, int textLength) {
 		// Your code goes here
-	}
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(initialText);
+
+        while (stringBuilder.length() < textLength) {
+            String key = stringBuilder.substring(stringBuilder.length() - windowLength);
+            stringBuilder.append(getRandomChar(CharDataMap.get(key)));
+        }
+        return stringBuilder.toString();
+    }
 
     /** Returns a string representing the map of this language model. */
 	public String toString() {
--- List.java
+++ List.java
@@ -30,11 +30,22 @@ public class List {
     /** GIVE Adds a CharData object with the given character to the beginning of this list. */
     public void addFirst(char chr) {
         // Your code goes here
+        first = new Node(new CharData(chr), first);
     }
     
     /** GIVE Textual representation of this list. */
     public String toString() {
         // Your code goes here
+        ListIterator lIterator = new ListIterator(first);
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("(");
+        while (lIterator.hasNext()) {
+            stringBuilder.append(" " + lIterator.next());
+        }
+        stringBuilder.append(" )");
+        return stringBuilder.toString();
     }
 
     /** Returns the index of the first CharData object in this list
@@ -42,6 +53,14 @@ public class List {
      *  or -1 if there is no such object in this list. */
     public int indexOf(char chr) {
         // Your code goes here
+        ListIterator lIterator = new ListIterator(first);
+
+        for (int index = 0; lIterator.hasNext(); index++) {
+            if (lIterator.next().equals(chr)) {
+                return index;
+            }
+        }
+        return -1;
     }
 
     /** If the given character exists in one of the CharData objects in this list,
@@ -49,6 +68,15 @@ public class List {
      *  given chr to the beginning of this list. */
     public void update(char chr) {
         // Your code goes here
+        ListIterator lIterator = new ListIterator(first);
+        while (lIterator.hasNext()) {
+            CharData current = lIterator.next();
+            if (current.equals(chr)) {
+                current.count++;
+                return;
+            }
+        }
+        addFirst(chr);
     }
 
     /** GIVE If the given character exists in one of the CharData objects
@@ -56,13 +84,34 @@ public class List {
      *  true. Otherwise, returns false. */
     public boolean remove(char chr) {
         // Your code goes here
+        if (first.cp.equals(chr)) {
+            first = first.next;
+        }
+
+        Node previous = first;
+        do {
+            Node current = previous.next;
+            if (current.cp.equals(chr)) {
+                previous.next = current.next;
+                return true;
+            }
+            previous = current;
+        } while (previous.next != null);
+        return false;
     }
 
     /** Returns the CharData object at the specified index in this list. 
      *  If the index is negative or is greater than the size of this list, 
      *  throws an IndexOutOfBoundsException. */
     public CharData get(int index) {
-        // Your code goes here
+        Node current = first;
+        for (int i = 0; current.next != null; ++i) {
+            if (i == index) {
+                return current.cp;
+            }
+            current = current.next;
+        }
+        throw new IndexOutOfBoundsException();
     }
 
     /** Returns an array of CharData objects, containing all the CharData objects in this list. */
